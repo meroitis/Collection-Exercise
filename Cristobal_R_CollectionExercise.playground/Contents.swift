bbf01604725f8985fba58e09d4cbf09324ae9c01
meroitis <67976213+meroitis@users.noreply.github.com>
2020-07-15 14:08:00 -0500

    Add files via upload

---
+++ Cristobal_R_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Collection list
+var videogameCollection : [String] = [ "Bo2", "Sao Fatal Bullet", "GTA V", "Call Of Duty Modern Warfare","Assasins Creed Unity"]
+print("My Videogame Collection contains \(videogameCollection.count)")
+
+//Added to collection
+videogameCollection += ["The Last Of Us 2"]
+videogameCollection += ["Left Behind", "Minecraft"]
+
+// New Print
+print("My Videogame Collection contains \(videogameCollection.count) games.")
+
